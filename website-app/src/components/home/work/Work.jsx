5bd7f0bd0e4b253f8ab6a03ffd7802dab1b71968
dong-geon-Lee <ch9716@naver.com>
2022-08-07 20:31:42 +0900

    work completed

--- website-app/src/components/home/work/Work.jsx
+++ website-app/src/components/home/work/Work.jsx
@@ -1,13 +1,80 @@
 import React from "react";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import IosShareIcon from "@mui/icons-material/IosShare";
+
+const Data = [
+  {
+    id: 1,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work1.jpg",
+  },
+  {
+    id: 2,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work2.jpg",
+  },
+  {
+    id: 3,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work3.jpg",
+  },
+  {
+    id: 4,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work4.jpg",
+  },
+  {
+    id: 5,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work5.jpg",
+  },
+  {
+    id: 6,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    cover: "./assets/work/work6.jpg",
+  },
+];
 
 const Work = () => {
   return (
-    <section className="work">
-      <div className="heading">
-        <h3>MY WORKS</h3>
-        <h1>Some of My Competed Projects</h1>
-      </div>
-    </section>
+    <>
+      <section className="work">
+        <div className="heading">
+          <h3>MY WORKS</h3>
+          <h1>Some of My Competed Projects</h1>
+        </div>
+
+        <div className="content">
+          {Data.map((val) => (
+            <>
+              <div className="box">
+                <div className="img">
+                  <img src={val.cover} alt="" />
+                </div>
+
+                <div className="overlay">
+                  <div className="text">
+                    <h2>{val.title}</h2>
+                    <p>{val.desc}</p>
+                  </div>
+
+                  <div className="icon">
+                    <FullscreenIcon className="iconWork" />
+                    <IosShareIcon className="iconWork" />
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
