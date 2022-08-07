0b0131799bfaae9febb1f252c6a14fe93b9ee484
dong-geon-Lee <ch9716@naver.com>
2022-08-07 16:02:32 +0900

    skill completed

--- website-app/src/components/pageContent/Homepages.jsx
+++ website-app/src/components/pageContent/Homepages.jsx
@@ -3,6 +3,7 @@ import About from "../home/about/About";
 import Branding from "../home/Branding";
 import Home from "../home/homes/Home";
 import Service from "../home/services/Service";
+import Skill from "../home/Skill";
 import Wrapper from "../home/Wrapper";
 
 const Homepages = () => {
@@ -13,6 +14,7 @@ const Homepages = () => {
       <About />
       <Service />
       <Wrapper />
+      <Skill />
     </>
   );
 };
