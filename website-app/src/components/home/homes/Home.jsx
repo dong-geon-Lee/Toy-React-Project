53318313be0b7fbf59384846cfa915372b1772c1
dong-geon-Lee <ch9716@naver.com>
2022-08-08 08:15:33 +0900

    change margin

--- website-app/src/components/home/homes/Home.jsx
+++ website-app/src/components/home/homes/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="right topMargin">
+          <div className="right topMargin just">
             <h1>
               I AM A <br /> WEB DESIGNER
             </h1>
