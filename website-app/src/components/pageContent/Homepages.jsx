6a89b8f172b54bfd6cee36c1c9a7310c90ef2429
dong-geon-Lee <ch9716@naver.com>
2022-08-07 14:59:17 +0900

    completed 04-services

--- website-app/src/components/pageContent/Homepages.jsx
+++ website-app/src/components/pageContent/Homepages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../home/about/About";
 import Branding from "../home/Branding";
 import Home from "../home/homes/Home";
+import Service from "../home/services/Service";
 
 const Homepages = () => {
   return (
@@ -9,6 +10,7 @@ const Homepages = () => {
       <Home />
       <Branding />
       <About />
+      <Service />
     </>
   );
 };
